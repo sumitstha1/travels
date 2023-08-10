2e59c5f45d63143d02f8905a339872f194cecb48
sumitstha1 <edchristian.inventor@gmail.com>
2023-08-11 03:11:01 +0545

    Complete

---
+++ src/Components/Main/Main.jsx
@@ -0,0 +1,165 @@
+import React, { useEffect } from 'react'
+import './main.css'
+import { HiOutlineLocationMarker, HiOutlineClipboardCheck } from 'react-icons/hi'
+import img from '../../Assets/img (1).jpg'
+import img2 from '../../Assets/img (2).jpg'
+import img3 from '../../Assets/img (3).jpg'
+import img4 from '../../Assets/img (4).jpg'
+import img5 from '../../Assets/img (5).jpg'
+import img6 from '../../Assets/img (6).jpg'
+import img7 from '../../Assets/img (7).jpg'
+import img8 from '../../Assets/img (8).jpg'
+import img9 from '../../Assets/img (9).jpg'
+
+import Aos from 'aos'
+import 'aos/dist/aos.css'
+
+const Data = [
+  {
+    id: 1,
+    imgSrc: img,
+    destTitle: 'Bora Bora',
+    location: 'New Zealand',
+    grade: 'CULTURAL RELAX',
+    fees: '$700',
+    description: 'The epitome of romance, Bora Bora is one of the best travel destinations in the World. This place is known for its luxurious stays and adventurous activities.'
+  },
+
+  {
+    id: 2,
+    imgSrc: img2,
+    destTitle: 'Machu Picchu',
+    location: 'Peru',
+    grade: 'CULTURAL RELAX',
+    fees: '$700',
+    description: 'Huayna Picchu is a mountain in Peru, rising over Machu Picchu, the so-called Lost City of Incas. This place is popular among tourists as the sunrise from the sun gate is simply spectacular.'
+  },
+
+  {
+    id: 3,
+    imgSrc: img3,
+    destTitle: 'Great Barrier Reef',
+    location: 'Australia',
+    grade: 'CULTURAL RELAX',
+    fees: '$700',
+    description: 'Huayna Picchu is a mountain in Peru, rising over Machu Picchu, the so-called Lost City of Incas. This place is popular among tourists as the sunrise from the sun gate is simply spectacular.'
+  },
+
+  {
+    id: 4,
+    imgSrc: img4,
+    destTitle: 'Coppadocia',
+    location: 'Turkey',
+    grade: 'CULTURAL RELAX',
+    fees: '$800',
+    description: 'Huayna Picchu is a mountain in Peru, rising over Machu Picchu, the so-called Lost City of Incas. This place is popular among tourists as the sunrise from the sun gate is simply spectacular.'
+  },
+
+  {
+    id: 5,
+    imgSrc: img5,
+    destTitle: 'Guanajuato',
+    location: 'Mexico',
+    grade: 'CULTURAL RELAX',
+    fees: '$1100',
+    description: 'Huayna Picchu is a mountain in Peru, rising over Machu Picchu, the so-called Lost City of Incas. This place is popular among tourists as the sunrise from the sun gate is simply spectacular.'
+  },
+
+  {
+    id: 6,
+    imgSrc: img6,
+    destTitle: 'Cinque Terre',
+    location: 'Italy',
+    grade: 'CULTURAL RELAX',
+    fees: '$840',
+    description: 'Huayna Picchu is a mountain in Peru, rising over Machu Picchu, the so-called Lost City of Incas. This place is popular among tourists as the sunrise from the sun gate is simply spectacular.'
+  },
+
+  {
+    id: 7,
+    imgSrc: img7,
+    destTitle: 'Angkor Wat',
+    location: 'Cambodia',
+    grade: 'CULTURAL RELAX',
+    fees: '$790',
+    description: 'Huayna Picchu is a mountain in Peru, rising over Machu Picchu, the so-called Lost City of Incas. This place is popular among tourists as the sunrise from the sun gate is simply spectacular.'
+  },
+
+  {
+    id: 8,
+    imgSrc: img8,
+    destTitle: 'Tajmahal',
+    location: 'India',
+    grade: 'CULTURAL RELAX',
+    fees: '$700',
+    description: 'Huayna Picchu is a mountain in Peru, rising over Machu Picchu, the so-called Lost City of Incas. This place is popular among tourists as the sunrise from the sun gate is simply spectacular.'
+  },
+
+  {
+    id: 9,
+    imgSrc: img9,
+    destTitle: 'Machu Picchu',
+    location: 'Peru',
+    grade: 'CULTURAL RELAX',
+    fees: '$700',
+    description: 'Huayna Picchu is a mountain in Peru, rising over Machu Picchu, the so-called Lost City of Incas. This place is popular among tourists as the sunrise from the sun gate is simply spectacular.'
+  },
+]
+
+const Main = () => {
+
+  useEffect(() => {
+    Aos.init({duration: 2000})
+  }, [])
+
+  return (
+    <section className='main container section'>
+      <div className="secTitle">
+        <h3 data-aos='fade-right' className="title">
+          Most visited destinations
+        </h3>
+      </div>
+
+      <div className="secContent grid">
+        {
+          Data.map(({ id, imgSrc, destTitle, location, grade, fees, description }) => {
+            return (
+              <div data-aos='fade-up' key={id} className="singleDestination">
+                <div className="imageDiv">
+                  <img src={imgSrc} alt={destTitle} />
+                </div>
+
+                <div className="cardInfo">
+                  <h4 className="destTitle">{destTitle}</h4>
+                  <span className="continent flex">
+                    <HiOutlineLocationMarker className='icon' />
+                    <span className="name">{location}</span>
+                  </span>
+
+                  <div className="fees flex">
+                    <div className="grade">
+                      <span>{grade}<small>+1</small></span>
+                    </div>
+                    <div className="price">
+                      <h5>{fees}</h5>
+                    </div>
+                  </div>
+
+                  <div className="desc">
+                    <p>{description}</p>
+                  </div>
+
+                  <button className="btn flex">
+                    DETAILS <HiOutlineClipboardCheck className='icon' />
+                  </button>
+                </div>
+              </div>
+            )
+          })
+        }
+      </div>
+    </section>
+  )
+}
+
+export default Main
